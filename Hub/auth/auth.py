c776bda948aaba8757c3fa15ea906d56e29a377d
kevinkons <kevin.kons@hotmail.com>
2024-07-02 17:40:48 -0300

    Removing unused imports

--- Hub/auth/auth.py
+++ Hub/auth/auth.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Depends, HTTPException, status, Cookie
+from fastapi import Depends, HTTPException, status, Cookie
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
