3b207a709875b6927b5cc97e113c7cbbf8abd496
Andre Borges Medeiros <andborges@gmail.com>
2024-07-03 16:36:25 -0300

    Initial commit for protobuff and parser

---
+++ Parser/libs/pika_client.py
@@ -0,0 +1,22 @@
+import aio_pika
+
+class PikaClient:
+    def __init__(self, rabbit_url):
+        self.rabbit_url = rabbit_url
+
+    async def consume(self, queue_name, callback, loop):
+        connection = await aio_pika.connect_robust(self.rabbit_url, loop=loop)
+        channel = await connection.channel()
+        queue = await channel.declare_queue(queue_name)
+
+        async def process_incoming_message(message):
+            body = message.body
+
+            if body:
+                await message.ack()
+                callback(body)
+
+        await queue.consume(process_incoming_message)
+
+        return connection
+    
---
+++ Parser/main.py
@@ -0,0 +1,23 @@
+import asyncio
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import os
+from libs.pika_client import PikaClient
+
+load_dotenv()
+app = FastAPI()
+pika_client = PikaClient(os.environ.get('RABBITMQ_URL'))
+
+@app.get("/") 
+async def main_route():
+  return {"message": "Hey, It is me Parser: " + os.environ.get('RABBITMQ_URL')}
+
+@app.on_event('startup')
+async def startup():
+  loop = asyncio.get_running_loop()
+
+  def callback(body):
+    print(f" [x] Received {body}")
+
+  task = loop.create_task(pika_client.consume('graphs_queue', callback, loop))
+  await task
