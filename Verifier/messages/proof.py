b7d3d5f88ba489aa770c489f9004070fe2750780
Andre Borges Medeiros <andborges@gmail.com>
2024-07-30 16:28:21 -0300

    Update proof message handling

--- Verifier/messages/proof.py
+++ Verifier/messages/proof.py
@@ -3,4 +3,4 @@ from services.verify_service import verify_proof
 
 async def handle_proof_message(body, consumer_id):
     payload = json.loads(body)
-    await verify_proof(consumer_id, payload.get("proof_request_id"), payload.get("proof_provider"), payload.get("proof"))
+    await verify_proof(consumer_id, payload.get("proof_request_id"), payload.get("circuit"), payload.get("proof"))
